55c87dbef2c0fb0e44546001fd8617b594ce8fd7
LSH <secondwarren@gmail.com>
2021-07-09 00:21:00 +0900

    Book 모델 생성

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Genre
+from .models import Author
+from .models import Book
 
 # Register your models here.
 admin.site.register(Genre)
+admin.site.register(Author)
+admin.site.register(Book)
--- catalog/models.py
+++ catalog/models.py
@@ -14,4 +14,23 @@ class Author(models.Model):
   first_name = models.CharField(max_length=100)
   last_name = models.CharField(max_length=100)
   date_of_birth = models.DateField(null=True, blank=True)
-  date_of_birth = models.DateField(null=True, blank=True)
+  date_of_death = models.DateField("Died", null=True, blank=True)
+
+  class Meta:
+    ordering = ["last_name", "first_name"]
+
+  def __str__(self):
+    return f"{self.last_name}, {self.first_name}"
+
+class Book(models.Model):
+  title = models.CharField(max_length=200)
+  author = models.ForeignKey("Author", on_delete=models.SET_NULL, null=True)
+  summary = models.TextField(max_length=1000, help_text="책의 설명을 입력하세요")
+  isbn = models.CharField(
+    "ISBN",
+     max_length=13,
+     help_text="13자리 ISBN을 입력하세요")
+  genre = models.ManyToManyField(Genre, help_text="책의 장르를 고르세요")
+
+  def __str__(self):
+      return self.title
